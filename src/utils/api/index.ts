1d613a26bf72c04b2d40bf2c7f335774d9a33fad
frimpsss <afrimpong031@gmail.com>
2024-05-04 01:34:29 +0000

    fix refreah token error

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,45 +1,139 @@
+// import axios, { InternalAxiosRequestConfig } from "axios";
+// import { Cookies } from "react-cookie";
+// const cookie = new Cookies();
+
+// let isRefreshing = false;
+// let refreshQueue: (() => Promise<any>)[] = [];
+
+// export const _ = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_api_base_url as string,
+//   withCredentials: true,
+// });
+
+// export async function getRefreshToken() {
+//   try {
+//     const response = await _.get("/refresh");
+//     if (response?.data?.status) {
+//       const old_cookie = cookie.get("user");
+//       cookie.set("user", {
+//         username: old_cookie?.username,
+//         token: response?.data?.access_token,
+//       });
+
+//       return response?.data?.access_token;
+//     }
+//   } catch (error) {
+//     //og(error);
+//     return Promise.reject(error);
+//   }
+// }
+
+// _.interceptors.request.use(
+//   async (
+//     _c: InternalAxiosRequestConfig
+//   ): Promise<InternalAxiosRequestConfig> => {
+//     const access_token = cookie.get("user")?.token;
+
+//     if (access_token) {
+//       _c.headers.Authorization = `Bearer ${access_token}`;
+//     }
+//     return _c;
+//   },
+//   (error) => {
+//     console.error("attarch brearer token: ", error);
+//     return Promise.reject(error);
+//   }
+// );
+// _.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config as InternalAxiosRequestConfig & {
+//       _retry?: boolean;
+//     };
+//     if (
+//       error.response.data?.message === "expired_token" &&
+//       !originalRequest?._retry
+//     ) {
+//       originalRequest._retry = true;
+//       try {
+
+//         const newAccessToken = await getRefreshToken();
+
+//         originalRequest.headers.Authorization = "Bearer " + newAccessToken;
+//         //og(originalRequest);
+//         return _(originalRequest);
+//       } catch (refreshError) {
+//         console.error("Failed to refresh access token: ", refreshError);
+//         window.location.href = "/login";
+//         return Promise.reject(error);
+//       }
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+// _.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config as InternalAxiosRequestConfig & {
+//       _retry?: boolean;
+//     };
+//     if (error.response.status == 401 && !originalRequest?._retry) {
+//       originalRequest._retry = true;
+//       cookie.remove("user");
+//       window.location.href = "/login";
+//     }
+
+//     return Promise.reject(error);
+//   }
+// );
+// _.interceptors.request.use(
+//   (config: any) => {
+//     if (config.method === "options") {
+//       return Promise.resolve({ status: 200 });
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     console.error("option request:", error);
+//     return Promise.reject(error);
+//   }
+// );
+
 import axios, { InternalAxiosRequestConfig } from "axios";
 import { Cookies } from "react-cookie";
+
 const cookie = new Cookies();
+
+let isRefreshing = false;
+let refreshQueue: (() => Promise<any>)[] = [];
+
 export const _ = axios.create({
   baseURL: process.env.NEXT_PUBLIC_api_base_url as string,
   withCredentials: true,
 });
 
-export async function getRefreshToken() {
-  try {
-    const response = await _.get("/refresh");
-    if (response?.data?.status) {
-      const old_cookie = cookie.get("user");
-      cookie.set("user", {
-        username: old_cookie?.username,
-        token: response?.data?.access_token,
-      });
-
-      return response?.data?.access_token;
-    }
-  } catch (error) {
-    //og(error);
-    return Promise.reject(error);
-  }
-}
-
 _.interceptors.request.use(
   async (
-    _c: InternalAxiosRequestConfig
+    config: InternalAxiosRequestConfig
   ): Promise<InternalAxiosRequestConfig> => {
     const access_token = cookie.get("user")?.token;
 
     if (access_token) {
-      _c.headers.Authorization = `Bearer ${access_token}`;
+      config.headers.Authorization = `Bearer ${access_token}`;
     }
-    return _c;
+    return config;
   },
   (error) => {
-    console.error("attarch brearer token:", error);
+    console.error("attach bearer token:", error);
     return Promise.reject(error);
   }
 );
+
 _.interceptors.response.use(
   (response) => {
     return response;
@@ -48,45 +142,67 @@ _.interceptors.response.use(
     const originalRequest = error.config as InternalAxiosRequestConfig & {
       _retry?: boolean;
     };
+
     if (
       error.response.data?.message === "expired_token" &&
       !originalRequest?._retry
     ) {
       originalRequest._retry = true;
-      try {
-        const newAccessToken = await getRefreshToken();
 
-        originalRequest.headers.Authorization = "Bearer " + newAccessToken;
-        //og(originalRequest);
-        return _(originalRequest);
-      } catch (refreshError) {
-        console.error("Failed to refresh access token: ", refreshError);
-        window.location.href = "/login";
-        return Promise.reject(error);
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          const newAccessToken = await getRefreshToken();
+          cookie.set("user", {
+            ...cookie.get("user"),
+            token: newAccessToken,
+          });
+
+          // Retry the original request with the new token
+          originalRequest.headers.Authorization = "Bearer " + newAccessToken;
+          return _(originalRequest);
+        } catch (refreshError) {
+          console.error("Failed to refresh access token: ", refreshError);
+          window.location.href = "/login";
+          return Promise.reject(error);
+        } finally {
+          isRefreshing = false;
+        }
+      } else {
+        // If there is already a refresh request in progress, queue the original request
+        return new Promise((resolve, reject) => {
+          refreshQueue.push(async () => {
+            try {
+              const newAccessToken = await getRefreshToken();
+              cookie.set("user", {
+                ...cookie.get("user"),
+                token: newAccessToken,
+              });
+
+              // Retry the original request with the new token
+              originalRequest.headers.Authorization =
+                "Bearer " + newAccessToken;
+              const response = await _(originalRequest);
+              resolve(response);
+            } catch (refreshError) {
+              console.error("Failed to refresh access token: ", refreshError);
+              window.location.href = "/login";
+              reject(error);
+            }
+          });
+        });
       }
-    }
-
-    return Promise.reject(error);
-  }
-);
-_.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config as InternalAxiosRequestConfig & {
-      _retry?: boolean;
-    };
-    if (error.response.status == 401 && !originalRequest?._retry) {
-      originalRequest._retry = true;
+    } else if (error.response.status === 401 && !originalRequest?._retry) {
+      // Handle 401 Unauthorized error
       cookie.remove("user");
-      //og("401 error: ", error)
       window.location.href = "/login";
     }
 
     return Promise.reject(error);
   }
 );
+
+// Intercept option requests
 _.interceptors.request.use(
   (config: any) => {
     if (config.method === "options") {
@@ -99,3 +215,15 @@ _.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+// Function to get new refresh token
+async function getRefreshToken() {
+  try {
+    const response = await _.get("/refresh");
+    if (response?.data?.status) {
+      return response?.data?.access_token;
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
