9b80d8aaac4cbb24c524d5ba46943b27c2738b84
frimpsss <afrimpong031@gmail.com>
2024-08-14 14:57:04 +0000

    hot fix init

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,109 +1,3 @@
-// import axios, { InternalAxiosRequestConfig } from "axios";
-// import { Cookies } from "react-cookie";
-// const cookie = new Cookies();
-
-// let isRefreshing = false;
-// let refreshQueue: (() => Promise<any>)[] = [];
-
-// export const _ = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_api_base_url as string,
-//   withCredentials: true,
-// });
-
-// export async function getRefreshToken() {
-//   try {
-//     const response = await _.get("/refresh");
-//     if (response?.data?.status) {
-//       const old_cookie = cookie.get("user");
-//       cookie.set("user", {
-//         username: old_cookie?.username,
-//         token: response?.data?.access_token,
-//       });
-
-//       return response?.data?.access_token;
-//     }
-//   } catch (error) {
-//     //og(error);
-//     return Promise.reject(error);
-//   }
-// }
-
-// _.interceptors.request.use(
-//   async (
-//     _c: InternalAxiosRequestConfig
-//   ): Promise<InternalAxiosRequestConfig> => {
-//     const access_token = cookie.get("user")?.token;
-
-//     if (access_token) {
-//       _c.headers.Authorization = `Bearer ${access_token}`;
-//     }
-//     return _c;
-//   },
-//   (error) => {
-//     console.error("attarch brearer token: ", error);
-//     return Promise.reject(error);
-//   }
-// );
-// _.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config as InternalAxiosRequestConfig & {
-//       _retry?: boolean;
-//     };
-//     if (
-//       error.response.data?.message === "expired_token" &&
-//       !originalRequest?._retry
-//     ) {
-//       originalRequest._retry = true;
-//       try {
-
-//         const newAccessToken = await getRefreshToken();
-
-//         originalRequest.headers.Authorization = "Bearer " + newAccessToken;
-//         //og(originalRequest);
-//         return _(originalRequest);
-//       } catch (refreshError) {
-//         console.error("Failed to refresh access token: ", refreshError);
-//         window.location.href = "/login";
-//         return Promise.reject(error);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-// _.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config as InternalAxiosRequestConfig & {
-//       _retry?: boolean;
-//     };
-//     if (error.response.status == 401 && !originalRequest?._retry) {
-//       originalRequest._retry = true;
-//       cookie.remove("user");
-//       window.location.href = "/login";
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-// _.interceptors.request.use(
-//   (config: any) => {
-//     if (config.method === "options") {
-//       return Promise.resolve({ status: 200 });
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     console.error("option request:", error);
-//     return Promise.reject(error);
-//   }
-// );
-
 import axios, { InternalAxiosRequestConfig } from "axios";
 import { Cookies } from "react-cookie";
 
@@ -160,8 +54,10 @@ _.interceptors.response.use(
 
           // Retry the original request with the new token
           originalRequest.headers.Authorization = "Bearer " + newAccessToken;
+          processQueue(null, newAccessToken);
           return _(originalRequest);
         } catch (refreshError) {
+          processQueue(refreshError, null);
           console.error("Failed to refresh access token: ", refreshError);
           window.location.href = "/login";
           return Promise.reject(error);
@@ -227,3 +123,14 @@ async function getRefreshToken() {
     return Promise.reject(error);
   }
 }
+
+const processQueue = (error: any, token: string | null) => {
+  refreshQueue.forEach((callback: any) => {
+    if (token) {
+      callback(token);
+    } else {
+      callback(error);
+    }
+  });
+  refreshQueue = [];
+};
